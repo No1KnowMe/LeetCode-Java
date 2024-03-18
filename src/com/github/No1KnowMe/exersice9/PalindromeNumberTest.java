33e184502e744a024798eb8efeb7eb9601ac6082
darthrazif@gmail.com <darthrazif@gmail.com>
2024-03-18 16:21:23 +0300

    Exersice 9 (Palindrome Number) Test updated

--- src/com/github/No1KnowMe/exersice9/PalindromeNumberTest.java
+++ src/com/github/No1KnowMe/exersice9/PalindromeNumberTest.java
@@ -1,41 +1,22 @@
 package com.github.No1KnowMe.exersice9;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class PalindromeNumberTest {
 
-    @Test
-    void checkPalindrome_TEST_1() {
-        boolean testOutput = true;
-        int testInput = 121;
-        boolean actualOutput = PalindromeNumber.checkPalindrome(testInput);
-
-        assertEquals(testOutput, actualOutput);
-        String result = (testOutput == actualOutput) ? "TEST 1 PASSED" : "TEST 1 FAILED";
-        System.out.println(result);
-    }
-
-    @Test
-    void checkPalindrome_TEST_2() {
-        boolean testOutput = false;
-        int testInput = -121;
-        boolean actualOutput = PalindromeNumber.checkPalindrome(testInput);
-
-        assertEquals(testOutput, actualOutput);
-        String result = (testOutput == actualOutput) ? "TEST 2 PASSED" : "TEST 2 FAILED";
-        System.out.println(result);
-    }
-
-    @Test
-    void checkPalindrome_TEST_3() {
-        boolean testOutput = false;
-        int testInput = 10;
-        boolean actualOutput = PalindromeNumber.checkPalindrome(testInput);
-
-        assertEquals(testOutput, actualOutput);
-        String result = (testOutput == actualOutput) ? "TEST 3 PASSED" : "TEST 3 FAILED";
-        System.out.println(result);
+    @DisplayName("Check a number if it's a palindrome.")
+    @ParameterizedTest(name = "Is it {0} a palindrome? {1}")
+    @CsvSource({
+            "121, true",
+            "-121, false",
+            "10, false"
+    })
+    void checkPalindrome(int number, boolean result) {
+        assertEquals(result, PalindromeNumber.checkPalindrome(number),
+                () -> "Is it " + number + " a palindrome? " + result);
     }
 }
